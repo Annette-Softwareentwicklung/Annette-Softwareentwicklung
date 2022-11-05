4097b0de14f82ca8a767822cd584b9e016c78982
Spammer92 <113533295+Spammer92@users.noreply.github.com>
2022-11-05 14:09:33 +0100

    Introdicton Section

---
+++ src/sections/introduction-section/introduction-section.jsx
@@ -0,0 +1,12 @@
+
+// ! Das importieren von css modules
+import styles from "./introduction-section.module.css";
+import logo from "./../../assets/images/logo.png";
+
+export function IntroductionsectionSection() {
+  return <section className={styles.introductionSection}>
+    <article>
+      <h2>Wir sind...</h2>
+    </article>
+  </section>
+}
