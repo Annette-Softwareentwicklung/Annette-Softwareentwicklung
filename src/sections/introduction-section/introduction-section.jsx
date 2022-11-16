3bd6ba63f39b6f896f924ed1941feb92c3a18f8a
Spammer92 <jonah.heinisch@gmail.com>
2022-11-16 16:54:46 +0100

    Zwischenstand Grundstruktur

--- src/sections/introduction-section/introduction-section.jsx
+++ src/sections/introduction-section/introduction-section.jsx
@@ -2,6 +2,7 @@
 // ! Das importieren von css modules
 import styles from "./introduction-section.module.css";
 import logo from "./../../assets/images/logo.png";
+import logocdia from "./../../assets/images/cdia_v_1_0_dark2x.png";
 
 export function IntroductionSection() {
   return <section className={styles.introductionSection}>
@@ -9,12 +10,12 @@ export function IntroductionSection() {
       <h2>Wir sind...</h2>
       <p>...eine Arbeitsgemeinschaft am Annette-von-Droste-Hülshoff-Gymnasium</p>
       <div></div>
-      <div class="flex">
-        <div class="flex-item">
-        <p>...und Teil des P</p>
+      <div className="flex">
+        <div className="flex-item">
+        <p>...und Teil des Projekts "Community der Informatischen AGs", einer Programmier-Gemeinschaft mit AGs am Koblenzer, Schloß und Annette-von-Droste-Hülshoff Gymnasium.</p>
         </div>
-        <div class="flex-item">
-          <img src="scr/assets/imagescdia_v_1_0_dark2x.png" alt="Community der Informatischen Ag's" />
+        <div className="flex-item">
+          <img src={logocdia} alt="Community der Informatischen Ag's" />
         </div>
       </div>
     </article>
