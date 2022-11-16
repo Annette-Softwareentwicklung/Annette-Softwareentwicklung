bee167d0886dfbd9c4bd80730e39f4fa14bd260e
Spammer92 <jonah.heinisch@gmail.com>
2022-11-16 16:37:51 +0100

    Zwischenstand Grundstruktur

--- src/sections/introduction-section/introduction-section.jsx
+++ src/sections/introduction-section/introduction-section.jsx
@@ -3,10 +3,20 @@
 import styles from "./introduction-section.module.css";
 import logo from "./../../assets/images/logo.png";
 
-export function IntroductionsectionSection() {
+export function IntroductionSection() {
   return <section className={styles.introductionSection}>
     <article>
       <h2>Wir sind...</h2>
+      <p>...eine Arbeitsgemeinschaft am Annette-von-Droste-Hülshoff-Gymnasium</p>
+      <div></div>
+      <div class="flex">
+        <div class="flex-item">
+        <p>...und Teil des P</p>
+        </div>
+        <div class="flex-item">
+          <img src="scr/assets/imagescdia_v_1_0_dark2x.png" alt="Community der Informatischen Ag's" />
+        </div>
+      </div>
     </article>
   </section>
 }
