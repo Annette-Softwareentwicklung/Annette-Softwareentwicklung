6ab6e7b89f0a6cb8da99de9f1225d1bff5233772
Spammer92 <jonah.heinisch@gmail.com>
2022-12-03 14:08:58 +0100

    404 Seite und Link Struktur

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,3 +1,3 @@
 // ! die Index Dateien sorgen dafür, dass der gesamte Content (also alle functional components) aus den angegebenen Ordnern exportiert werden.
-export * from "./landing-section"
-export * from "./team-section"
+export * from "./landing-section";
+export * from "./team-section";
