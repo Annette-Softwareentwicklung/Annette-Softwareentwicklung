2af6c812b1c6068d843ae50e7bad872219de1432
Eli-dd <90352951+Eli-dd@users.noreply.github.com>
2022-12-14 16:43:55 +0100

    project section erstellt (2. commit, weil es beim ersten Mal nicht geklappt hat)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Footer, Header } from "./components"; 
-import { LandingSection, TeamSection } from "./sections";
+import { LandingSection, TeamSection, ProjectsSection } from "./sections";
 // durch die index.js dateien, die das Exportieren der Inhalte zusammenbündelt, damit man dann alle components von "./components" importieren kann
 import "./globals.css"
 
@@ -9,6 +9,7 @@ function App() {
       <Header />
       <main>
         <LandingSection />
+        <ProjectsSection />
         <TeamSection />
       </main>
       <Footer />
--- src/sections/index.js
+++ src/sections/index.js
@@ -1,3 +1,4 @@
 // ! die Index Dateien sorgen dafür, dass der gesamte Content (also alle functional components) aus den angegebenen Ordnern exportiert werden.
 export * from "./landing-section"
 export * from "./team-section"
+export * from "./projects-section"
