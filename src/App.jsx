cfdc204c1e88e14a0dcf94afa97aec4d475fba07
Eli-dd <90352951+Eli-dd@users.noreply.github.com>
2023-01-11 17:02:14 +0100

    Den Header-Code in components/header verlegt

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,4 @@ function App() {
   );
 }
 
-function Header() {
-  return (
-    <table>
-		<tr>
-			<td><p>Annette Softwareentwicklung</p></td>
-			<td><p>Unser Team</p></td>
-			<td><p>Unsere Projekte</p></td>
-			<td><p>Kontakt</p></td>
-		</tr>
-		
-		<tr>
-			<td><h1>Entwickeln für moderne Zukunft</h1></td>
-			<td><img src=""></img></td>
-		</tr>
-	</table>
-	<hr>
-  );
-}
-
 export default App;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,5 +2,19 @@ import styles from "./header.module.css"
 
 export function Header() {
   return <header className={styles.header}>
+        <table>
+		<tr>
+			<td><p>Annette Softwareentwicklung</p></td>
+			<td><p>Unser Team</p></td>
+			<td><p>Unsere Projekte</p></td>
+			<td><p>Kontakt</p></td>
+		</tr>
+		
+		<tr>
+			<td><h1>Entwickeln für moderne Zukunft</h1></td>
+			<td><img src=""></img></td>
+		</tr>
+	</table>
+  <hr></hr>
   </header>
 }
