c7ebc05699be9fd976e5a31081ea83b728ac32c4
Eli-dd <90352951+Eli-dd@users.noreply.github.com>
2023-03-15 16:21:24 +0100

    Test-Commit für das Tutorial

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,7 +5,7 @@ import styles from "./footer.module.css"
 export function Footer() {
   return <footer className={styles.footer}>
     <h1>© Annette-Softwareentwicklung {new Date().getFullYear()}</h1>
-    <p>Mit Leidenschaft und Freude entwickelt. Von Elias Dörr, Jonah Heinisch, Nikos Jeronimo Rohmert, Jonas Erdmann, Rui Zhang</p>
+    <p>Mit Leidenschaft und Freude entwickelt. Von Elias Dörr, Jonah Heinisch, Nikos Jeronimo Rohmert, Jonas Erdmann, Rui Bang</p>
     <p>Email: <a href={"mailto:"+EMAIL}>{EMAIL}</a></p>     
 
   </footer>
