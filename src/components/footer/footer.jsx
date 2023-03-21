f9ff55cc85ef625a5921a00759b59c3231fee9e4
Eli-dd <90352951+Eli-dd@users.noreply.github.com>
2023-03-21 19:20:10 +0100

    Revert "Test-Commit für das Tutorial"
    This reverts commit c7ebc05699be9fd976e5a31081ea83b728ac32c4.

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,7 +6,7 @@ export function Footer() {
   return <footer className={styles.footer}>
     <button title="Zurück nach oben" onClick={scrollToTop}><CaretUp size={50} color="#3a3636" weight="thin" /></button>
     <h1>© Annette-Softwareentwicklung {new Date().getFullYear()}</h1>
-    <p>Mit Leidenschaft und Freude entwickelt. Von Elias Dörr, Jonah Heinisch, Nikos Jeronimo Rohmert, Jonas Erdmann, Rui Bang</p>
+    <p>Mit Leidenschaft und Freude entwickelt. Von Elias Dörr, Jonah Heinisch, Nikos Jeronimo Rohmert, Jonas Erdmann, Rui Zhang</p>
     <p>Email: <a href={"mailto:"+EMAIL}>{EMAIL}</a></p>     
 
   </footer>
